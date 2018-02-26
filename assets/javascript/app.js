d35978905709490564ca9b259519a3cd358c52ce
Mapi Pina <mpina09@gmail.com>
2018-02-26 18:58:11 -0500

    gah the correct counter is only working with question 1 and it's not even set up right so annoyed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,20 +6,33 @@ $(document).ready(function(){
     });
 
     //time to define some variables
+    //attempt two at creating event listener for checked
+   /* $(".question").click(function(){
+        $(this).find("input[type=radio]").prop("checked", "true");
+    }) */
 //need variable that defines the correct answers so that jQuery can match user input to what's in the object
-    var answer1 = $("input[type=radio][name=q1]:checked").val();
-    var answer2 = $("input[type=radio][name=q2]:checked").val();
-    var answer3 = $("input[type=radio][name=q3]:checked").val();
+  //  var oldanswer1 = $("input[type=radio][name=q1]:checked").val();
+  //  var oldanswer2 = $("input[type=radio][name=q2]:checked").val();
+  //  var oldanswer3 = $("input[type=radio][name=q3]:checked").val();
     var correctCounter = 0;
     var incorrectCounter = 0;
     var unansweredCounter = 0;
 // need an event listener to add checked to input selected
-    var makeChecked = function(){
-    	$("input").click()
-    }
+   /* var answer1b = $("input[type=radio][name=q1]").click(function(){
+        $("input[type=radio][name=q1]").prop("checked", true);
+        answer1 = answer1b;
+    }); */
+    var answer1 = $("input[type=radio][name=q1]").change(function(){
+        var isChecked = $(this).is(":checked");
+        if (isChecked === 'steamboat' || '1-dol' || 'tokyo') {
+            correctCounter++;
+        }
+    });
+
 
     //next step is to create function that checks to see if answer is correct, incorrect or unaswered & updates counter
-    var tally = function () {
+   /* var tally = function () {
+    	
     	// have to create if statement that defines what to do when userSelect matches with object in answers
     	if (answer1 === 'steamboat') {
     		correctCounter++;
@@ -31,17 +44,21 @@ $(document).ready(function(){
     		correctCounter++;
     	}
 
-    };
+    }; */
 
-
+    console.log(answer1);
+  //  console.log(answer2);
+  //  console.log(answer3);
     //clicking done with hide the quiz and show the last page
     $("#done").click(function(){
     	$(".page3").toggle();
     	// how the fuck do I call this function in order to update the correctCounter that appears on last page?
     	$("#done").toggle();
     	$(".page2").toggle();
-    	tally();
     	$("#correct").append(correctCounter);
+        console.log(correctCounter)
+        $("#incorrect").append(incorrectCounter);
+        $("#blank").append(unansweredCounter);
     });
 
 
