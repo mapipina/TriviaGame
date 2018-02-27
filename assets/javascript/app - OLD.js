5b1a64d1341ba7b1825bea8258c5f8e82150641d
Mapi Pina <mpina09@gmail.com>
2018-02-27 14:13:49 -0500

    trying to upload again because io page is old

--- assets/javascript/app - OLD.js
+++ assets/javascript/app - OLD.js
@@ -24,8 +24,58 @@ $(document).ready(function(){
     var correctCounter = 0;
     var incorrectCounter = 0;
     var unansweredCounter = 0;
+    //attempt two at creating event listener for checked
+   /* $(".question").click(function(){
+        $(this).find("input[type=radio]").prop("checked", "true");
+    }) */
+//need variable that defines the correct answers so that jQuery can match user input to what's in the object
+  //  var oldanswer1 = $("input[type=radio][name=q1]:checked").val();
+  //  var oldanswer2 = $("input[type=radio][name=q2]:checked").val();
+  //  var oldanswer3 = $("input[type=radio][name=q3]:checked").val();
 
-// create the answers checkers
+// need an event listener to add checked to input selected
+   /* var answer1b = $("input[type=radio][name=q1]").click(function(){
+        $("input[type=radio][name=q1]").prop("checked", true);
+        answer1 = answer1b;
+    }); */
+   /* var answer = $("input[type=radio]").change(function(){
+        var isChecked = $(this).is(":checked");
+        $(isChecked).val() = isChecked;
+        if (isChecked === 'steamboat' || '1-dol' || 'tokyo') {
+            correctCounter++;
+        }
+    }); */
+
+    // my 100th attempt at getting the counters correct
+  /*  var answers = function (){
+        for (var i = 1; i < 45; i++) {
+            var radios = $('q' + i);
+            for (var j = 0; j < radios.length; j++) {
+                var radio = radios[j];
+                if (radio.val() === 'steamboat' && radio.checked) {
+                    correctCounter++;
+                }
+            }
+            
+        }
+    }; */
+
+
+    //next step is to create function that checks to see if answer is correct, incorrect or unaswered & updates counter
+   /* var tally = function () {
+    	
+    	// have to create if statement that defines what to do when userSelect matches with object in answers
+    	if (answer1 === 'steamboat') {
+    		correctCounter++;
+    	};
+    	if (answer2 === '1-dol') {
+    		correctCounter++;
+    	};
+    	if (answer3 === 'tokyo') {
+    		correctCounter++;
+    	}
+
+    }; */
     $("input[name=q1]").click(function() {
         var val = $('input[name=q1]:checked').val();
         if (val === 'steamboat') {
@@ -55,7 +105,6 @@ $(document).ready(function(){
         } 
 
     });
-
     
     //clicking done with hide the quiz and show the last page
     $("#done").click(function(){
