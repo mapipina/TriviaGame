8ea7caf255f876981659cc1d4eef080c39f63223
Mapi Pina <mpina09@gmail.com>
2018-03-01 23:25:42 -0500

    holy moly got the unanswered questions working and adding a functional reset woo!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
-	//click start to get the game started, hide the start button, and show the quiz
+    //click start to get the game started, hide the start button, and show the quiz
     $("#start").click(function(){
         $(".page2").toggle();
         $("#start").toggle();
     });
 
     //one of the last things I'm doing is creating timer
-    var count=15;
+    var count = 31;
     var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
     function timer(){
-        count=count-1;
+        count = count-1;
         if (count <= 0) {
             clearInterval(counter);
             $("#done").click();
             return;
         }
         $("#timer").html('Time Remaining: ' + count + ' Seconds');
-     }
+     };
     // have the timer show on load
-    //$(".page2").load()
+  // $(".page2").load().timer();
 
     //time to define some variables
     var correctCounter = 0;
@@ -26,7 +26,42 @@ $(document).ready(function(){
     var unansweredCounter = 0;
 
 // create the answers checkers
-    $("input[name=q1]").click(function() {
+    $("#done").click(function(){
+        var val = $('input[name=q1]:checked').val();
+        if (val) {
+            if (val ==='steamboat') {
+                correctCounter++;
+            } else {
+                incorrectCounter++;
+            }
+        } else {
+            unansweredCounter++;
+        }
+        var val2 = $('input[name=q2]:checked').val();
+        if (val2) {
+            if (val2 ==='1-dol') {
+                correctCounter++;
+            } else {
+                incorrectCounter++;
+            }
+        } else {
+            unansweredCounter++;
+        }
+        var val3 = $('input[name=q3]:checked').val();
+        if (val3) {
+            if (val3 ==='tokyo') {
+                correctCounter++;
+            } else {
+                incorrectCounter++;
+            }
+        } else {
+            unansweredCounter++;
+        }
+
+
+    });
+   
+   /* $("input[name=q1]").click(function() {
         var val = $('input[name=q1]:checked').val();
         if (val === 'steamboat') {
             correctCounter++;
@@ -54,18 +89,24 @@ $(document).ready(function(){
             incorrectCounter++;
         } 
 
-    });
+    }); */
 
-    
+
+//need a reset function
+ /*   function reset(){
+        clearTimeout(timer);
+
+    }; */
     //clicking done with hide the quiz and show the last page
     $("#done").click(function(){
-    	$(".page3").toggle();
-    	// how the fuck do I call this function in order to update the correctCounter that appears on last page?
-    	$("#done").toggle();
-    	$(".page2").toggle();
-    	$("#correct").append(correctCounter);
+        $(".page3").toggle();
+        // how the fuck do I call this function in order to update the correctCounter that appears on last page?
+        $(".page2").remove();
+        $("#correct").append(correctCounter);
         $("#incorrect").append(incorrectCounter);
-        $("#blank").append(unansweredCounter);
+        console.log(unansweredCounter);
+       // reset();
+       $("#blank").append(unansweredCounter);
     });
 
 
